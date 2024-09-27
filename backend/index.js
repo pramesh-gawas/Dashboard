7c0cbd02375930fc2dd485931a667f38ce172328
pramesh-gawas <prameshgawas11@gmail.com>
2024-09-27 10:57:52 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const db = require("./db");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
 const router = require("./router");
 
 const PORT = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["Get", "POST", "PUT", "Delete"],
+  })
+);
 
 app.use("/", router);
 
