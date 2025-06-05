4313494e26fa91eedaec4dae1475195c3f7562f0
pramesh-gawas <prameshgawas11@gmail.com>
2025-06-05 17:59:28 +0530

    added index

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const db = require("./db");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const router = require("./routes/router");
+const passport = require("./middleware/auth");
+
+const bodyparser = require("body-parser");
+app.use(bodyparser.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+app.use("/", router);
+
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});
