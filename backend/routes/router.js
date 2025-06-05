326451751d57ca4f04112c3dad31cdeaf21cde80
pramesh-gawas <prameshgawas11@gmail.com>
2025-06-05 22:44:28 +0530

    vercel deploy

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const passport = require("../middleware/auth");
 const authMiddleWare = passport.authenticate("local", { session: false });
 const { GetAllTheData } = require("../controllers/filtercontrollers");
-const { createUser, CreateLog } = require("../controllers/usercontroller");
+const { createUser, CreateLog } = require("../controllers/userController");
 router.get("/home", authMiddleWare, GetAllTheData);
 router.post("/register", createUser);
 router.post("/login", authMiddleWare, CreateLog);
