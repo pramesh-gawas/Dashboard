0c37be12aa2fac68303260869f2c1ee178e1840d
pramesh-gawas <prameshgawas11@gmail.com>
2025-06-06 13:25:37 +0530

    routers updated

--- backend/router.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const passport = require("./Authentication/auth");
-const authMiddleWare = passport.authenticate("local", { session: false });
-const { GetAllTheData } = require("./controllers/filterController");
-const { createUser, CreateLog } = require("./controllers/userController");
-router.get("home", authMiddleWare, GetAllTheData);
-router.post("register", createUser);
-router.post("login", authMiddleWare, CreateLog);
-module.exports = router;
--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const passport = require("../middleware/auth");
 const authMiddleWare = passport.authenticate("local", { session: false });
-const { GetAllTheData } = require("../Controllers/filterController");
-const { createUser, CreateLog } = require("../Controllers/UserController");
+const { GetAllTheData } = require("../controllers/filterController");
+const { createUser, CreateLog } = require("../controllers/UserController");
 router.get("/home", authMiddleWare, GetAllTheData);
 router.post("/register", createUser);
 router.post("/login", authMiddleWare, CreateLog);
