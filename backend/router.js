dfa8c61d17ba526003c6c2a9b8c2d9fb19a8c72d
pramesh-gawas <prameshgawas11@gmail.com>
2025-06-06 11:54:11 +0530

    files removed

--- backend/router.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const passport = require("./Authentication/auth");
-const authMiddleWare = passport.authenticate("local", { session: false });
-const { GetAllTheData } = require("./Controllers/FilterControllers");
-const { createUser, CreateLog } = require("./Controllers/UserController");
-router.get("home", authMiddleWare, GetAllTheData);
-router.post("register", createUser);
-router.post("login", authMiddleWare, CreateLog);
-module.exports = router;
