e184c1abe99ebf704c1a0ed358a372c42e801aa1
pramesh-gawas <prameshgawas11@gmail.com>
2025-06-06 13:27:10 +0530

    routers updated

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const passport = require("../middleware/auth");
 const authMiddleWare = passport.authenticate("local", { session: false });
 const { GetAllTheData } = require("../controllers/filterController");
-const { createUser, CreateLog } = require("../controllers/UserController");
+const { createUser, CreateLog } = require("../controllers/userController");
 router.get("/home", authMiddleWare, GetAllTheData);
 router.post("/register", createUser);
 router.post("/login", authMiddleWare, CreateLog);
